27ddd1bb7b4d52036683dbc1c5db76264f18ecc7
Dongwon Kim <dongwon.kim@navercorp.com>
2016-05-15 00:38:34 +0900

    add vimrc linking script
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- mkln.sh
+++ mkln.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#################################################
+#### author: Dongwon Kim <dkim010@gmail.com>
+#### created at 2016.05.01
+#################################################
+
+### vimrc
+ln -s $PWD/.vimrc ~/
+
+### find scripts
 mkdir -p ~/bin
 FILES='gfind nfind xfind ofind'
 for f in $FILES; do
