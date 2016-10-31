0d11c3afb21e433433b7205518b6b2ea1b96942d
Dongwon Kim <dongwon.kim@navercorp.com>
2016-10-31 18:10:48 +0900

    add path to mkln.sh
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- mkln.sh
+++ mkln.sh
@@ -4,15 +4,18 @@
 #### created at 2016.05.01
 #################################################
 
+USER_HOME=~/.
+USER_HOME=~/users/dwkim
+
 ### conf files
 FILES='.vimrc .tmux.conf .gitconfig'
 for f in $FILES; do
-    ln -s $PWD/$f ~/$f
+    ln -s $PWD/$f $USER_HOME/$f
 done
 
 ### scripts
-mkdir -p ~/bin
+mkdir -p $USER_HOME/bin
 FILES='gfind nfind xfind ofind atx uenv adb_conn'
 for f in $FILES; do
-    ln -s $PWD/bin/$f ~/bin/$f
+    ln -s $PWD/bin/$f $USER_HOME/bin/$f
 done
