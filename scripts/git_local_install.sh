99447637019409add4b264f25c3d249ee2412786
Dongwon Kim <dongwon.kim@navercorp.com>
2016-11-13 22:03:03 +0900

    add git local install script
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

---
+++ scripts/git_local_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script for installing git on systems where you don't have root access.
+# git will be installed in $HOME/local/bin.
+
+# exit on error
+set -e
+
+GIT_VERSION=2.10.2
+
+# create our directories
+mkdir -p $HOME/local/bin $HOME/git_tmp
+cd $HOME/git_tmp
+
+# download source files for git
+REL=https://github.com/git/git/archive
+GIT_FILENAME=v${GIT_VERSION}
+curl -LO ${REL}/${GIT_FILENAME}.tar.gz
+
+# extract files, configure, and compile
+GIT_DIRNAME=git-2.10.2
+tar xvzf ${GIT_FILENAME}.tar.gz
+cd ${GIT_DIRNAME}
+make configure
+./configure --prefix=$HOME/local && make
+cp git $HOME/local/bin/
+cd ..
+
+# cleanup
+rm -rf $HOME/git_tmp
+
+echo "$HOME/local/bin/git is now available. You can optionally add $HOME/local/bin to your PATH."
