e61a9b84756cefae56c863432da5986570504eab
Dongwon Kim <dongwon.kim@navercorp.com>
2017-01-22 02:53:11 +0900

    git credential cache
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- scripts/pip_local_install.sh
+++ scripts/pip_local_install.sh
@@ -2,4 +2,3 @@ wget https://bootstrap.pypa.io/get-pip.py
 python2.7 get-pip.py --user
 rm get-pip.py
 pip install --user -I numpy pandas scikit-learn matplotlib readline
-## echo "export PATH=/home1/irteam/.local/bin:$PATH" >> ~/.bashrc
