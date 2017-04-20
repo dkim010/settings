8dd02860d15e7192868e24d379daab394fb550c5
Dongwon Kim <dongwon.kim@navercorp.com>
2017-04-20 16:09:14 +0900

    bug fix on pyenv
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- scripts/pyenv_local_install.sh
+++ scripts/pyenv_local_install.sh
@@ -5,5 +5,9 @@ set -e
 PYENV=https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer
 curl -L $PYENV | bash
 
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 pyenv install 2.7.13
 pyenv install 3.6.1
