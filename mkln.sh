c84a3422be7bc265aa43d88258ba861f6fe9efff
Dongwon Kim <dongwon.kim@navercorp.com>
2017-05-09 20:16:52 +0900

    add screenrc into mkln.sh
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- mkln.sh
+++ mkln.sh
@@ -7,7 +7,7 @@
 USER_HOME=~/.
 
 ### conf files
-FILES='.vim .vimrc .tmux.conf .gitconfig .pythonrc'
+FILES='.vim .vimrc .tmux.conf .gitconfig .pythonrc .screenrc'
 for f in $FILES; do
     ln -s $PWD/$f $USER_HOME/
 done
