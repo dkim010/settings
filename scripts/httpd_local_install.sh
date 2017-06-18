3c3e3a25c7eb60e53590ed103efc9b2dc39ed117
Dongwon Kim <dongwon.kim@navercorp.com>
2017-06-18 13:11:57 +0900

    add apr into httpd install
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- scripts/httpd_local_install.sh
+++ scripts/httpd_local_install.sh
@@ -6,17 +6,31 @@ set -e
 VERSION=2.4.25
 TARGET=httpd-$VERSION
 DST=$HOME/opt/$TARGET-bin
-WWW=$HOME/www
 
 function download(){
     cd $HOME/opt
     curl -LO http://mirror.navercorp.com/apache//httpd/${TARGET}.tar.bz2
     tar xf ${TARGET}.tar.bz2
+
+    curl -LO http://apache.mirror.cdnetworks.com//apr/apr-1.6.2.tar.gz
+    curl -LO http://apache.mirror.cdnetworks.com//apr/apr-util-1.6.0.tar.gz
 }
 
 function install(){
     cd $TARGET
+
+    # install apr
+    cd srclib
+    tar xf ../../apr-1.6.2.tar.gz
+    mv apr-1.6.2 apr
+    # install apr-util
+    tar xf ../../apr-util-1.6.0.tar.gz
+    mv apr-util-1.6.0 apr-util
+    # pcre -> yum install pcre-devel
+
+    cd ..
     ./configure \
+        --with-included-apr \
         --prefix=$DST
     make
     make install
@@ -24,10 +38,6 @@ function install(){
 
 download
 install
-
-# WWW directory
-mkdir -p $WWW
-cp $DST/htdocs/index.html $WWW/
 
 # Configuration
 cd $DST/conf
