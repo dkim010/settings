4c51f20550d16cced4984292ef73a444e64ba946
Dongwon Kim <dongwon.kim@navercorp.com>
2017-08-01 13:12:41 +0900

    add otx to use default tmux/socket
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- mkln.sh
+++ mkln.sh
@@ -14,7 +14,7 @@ done
 
 ### scripts
 mkdir -p $USER_HOME/bin
-FILES='gfind nfind xfind ofind atx uenv adb_conn run_www'
+FILES='gfind nfind xfind ofind atx otx uenv adb_conn run_www'
 for f in $FILES; do
     ln -s $PWD/bin/$f $USER_HOME/bin/$f
 done
