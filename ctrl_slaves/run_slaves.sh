3e8f9842df0704d0c1d6ef53d7aee160b7f14afa
Dongwon Kim <dongwon.kim@navercorp.com>
2017-12-23 13:53:41 +0900

    remove ctrl_slaves
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- ctrl_slaves/run_slaves.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PWD=$(cd `dirname $0` ; pwd)
-for slave in `egrep -v "^#" $PWD/slaves`
-do
-        echo "ssh $slave " $"${@// /\\ }" "..." 1>&2
-        ssh $slave $"${@// /\\ }" \
-        2>&1 | sed "s/^/$slave: /" &
-done
--- ctrl_slaves/ssh-host-reg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for slave in `cat slaves`
-do
-        ./ssh-host-reg.exp $slave
-done
