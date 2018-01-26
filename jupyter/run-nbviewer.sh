1a8492b3c02b222a127c2e57a8720eb17c5ad8f7
Dongwon Kim <dongwon.kim@navercorp.com>
2018-01-27 00:48:47 +0900

    add comments for nbviewer
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- jupyter/run-nbviewer.sh
+++ jupyter/run-nbviewer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Prepare
+## https://github.com/jupyter/nbviewer#local-installation
+## irteamsu $ sudo yum install libmemcached-devel openssl-devel pandoc libevent-devel
+## irteam $ pip install nbviewer
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 python -m nbviewer --localfiles=$HOME/dev/repo/jupyter --no-cache
