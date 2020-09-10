e106ec1d5d7c51a36351d4542b5dc09c91fc905e
Dongwon Kim <dongwon.kim@navercorp.com>
2020-09-10 15:10:24 +0900

    create symbolic link forcefully
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- mkln.sh
+++ mkln.sh
@@ -9,12 +9,12 @@ USER_HOME=~/.
 ### conf files
 FILES='.vim .vimrc .tmux.conf .gitconfig .pythonrc .screenrc'
 for f in $FILES; do
-    ln -s $PWD/$f $USER_HOME/
+    ln -fs $PWD/$f $USER_HOME/
 done
 
 ### scripts
 mkdir -p $USER_HOME/bin
 FILES='gfind nfind xfind ofind atx otx uenv adb_conn run_www gzwc dclean'
 for f in $FILES; do
-    ln -s $PWD/bin/$f $USER_HOME/bin/$f
+    ln -fs $PWD/bin/$f $USER_HOME/bin/$f
 done
