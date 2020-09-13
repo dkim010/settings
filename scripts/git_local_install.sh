ca4d85cfc011734ba4949b5b8861616dfcdccdaf
Dongwon Kim <dongwon.kim@navercorp.com>
2020-09-13 11:43:13 +0900

    use opt/git for local git
    Signed-off-by: Dongwon Kim <dongwon.kim@navercorp.com>

--- scripts/git_local_install.sh
+++ scripts/git_local_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script for installing git on systems where you don't have root access.
-# git will be installed in $HOME/local/bin.
+# git will be installed in $HOME/opt/git.
 
 # exit on error
 set -e
@@ -9,7 +9,7 @@ set -e
 GIT_VERSION=2.20.1
 
 # create our directories
-mkdir -p $HOME/local/bin $HOME/git_tmp
+mkdir -p $HOME/git_tmp $HOME/opt
 cd $HOME/git_tmp
 
 # download source files for git
@@ -22,11 +22,15 @@ GIT_DIRNAME=git-${GIT_VERSION}
 tar xvzf ${GIT_FILENAME}.tar.gz
 cd ${GIT_DIRNAME}
 make configure
-./configure --prefix=$HOME/local && make all
+./configure --prefix=$HOME/opt/$GIT_DIRNAME && make all
 make install
 cd ..
+
+# link
+cd $HOME/opt
+ln -s $GIT_DIRNAME git
 
 # cleanup
 rm -rf $HOME/git_tmp
 
-echo "$HOME/local/bin/git is now available. You can optionally add $HOME/local/bin to your PATH."
+echo "$HOME/opt/git/bin/git is now available. You can optionally add $HOME/opt/git/bin to your PATH."
