4b1f3b410f32b12f6d66ea4fe71235d20a41b36d
Dongwon Kim <dkim010@gmail.com>
2023-10-18 20:13:18 +0900

    remove unused files
    Signed-off-by: Dongwon Kim <dkim010@gmail.com>

--- daemon/run-jupyter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#jt -t grade3 -fs 9 -f dejavu -cellw 1200 -lineh 120
-jt -t chesterish -fs 9 -f dejavu -cellw 1200 -lineh 120 -T
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CUDA_VISIBLE_DEVICES=$JUPYTER_GPU jupyter notebook \
-    --config $DIR/jupyter_notebook_config.py \
-    --port 10080
--- daemon/run-tensorboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-TB_DEPOT=$HOME/dev/depot/tensorboard
-mkdir -p $TB_DEPOT
-tensorboard \
-    --logdir=$TB_DEPOT \
-    --port 10082
